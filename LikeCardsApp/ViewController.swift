a29980b10a9a540620c11e479671da33d48afafc
alabeeb <alabeeb81@gmail.com>
2023-02-14 21:19:39 +0200

    All modules commit

--- LikeCardsApp/ViewController.swift
+++ LikeCardsApp/ViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import GamingModule
+import StoreModule
+import ChatModule
 
 class ViewController: UIViewController {
 
@@ -14,6 +17,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func storeTapped(_ sender: Any) {
+        let bundle = Bundle(for: StoreViewController.self)
+        self.navigationController?.pushViewController(StoreViewController(nibName: "StoreViewController", bundle: bundle), animated: true)
+    }
+    
+    @IBAction func chatTapped(_ sender: Any) {
+        let bundle = Bundle(for: ChatViewController.self)
+        self.navigationController?.pushViewController(ChatViewController(nibName: "ChatViewController", bundle: bundle), animated: true)
+    }
+    
+    @IBAction func gamingTapped(_ sender: Any) {
+        let bundle = Bundle(for: GammingViewController.self)
+        self.navigationController?.pushViewController(GammingViewController(nibName: "GammingViewController", bundle: bundle), animated: true)
+    }
+    
 }
 
